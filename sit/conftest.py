429f021ef58f4cc64382a2bc40fe42ebd9a7be28
Thomas Ecuer <slashdevsda@gmail.com>
2017-10-13 00:37:48 +0200

    add a conftest.py file to trick pytest.

---
+++ sit/conftest.py
@@ -0,0 +1,6 @@
+# this file tricks pytest to load
+# the application package before loading tests,
+# allowing imports without messing with sys.path.
+#
+# taken from
+# https://stackoverflow.com/questions/20971619/ensuring-py-test-includes-the-application-directory-in-sys-path
